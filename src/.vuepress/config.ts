969feaed477733c05ba05355264fff4e2a5108b7
wumin <wumin@qq.com>
2025-02-27 14:09:57 +0800

    feat: 添加logo字体配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,11 +6,17 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "NINEBABY BLOG",
+  title: "NINEBABY",
   description: "NINEBABY BLOG",
-
   theme,
-
+  head: [
+    [
+      'link', {rel:'icon', href:'/images/logo.svg'}
+    ],
+    [
+      'link', {rel:'stylesheet', href:'/assets/css/index.css'}
+    ]
+  ],
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
